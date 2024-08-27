4d27d1e48f32e7854df12b11a2e64f6b7b558992
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2024-08-27 03:09:54 +0200

    chore: asset logo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import InjectiveLogo from "./components/assets/injectiveLogo.vue";
 
 const selectedView = ref("deposit");
 const addBgClasses = false;
@@ -13,17 +14,20 @@ const addBgClasses = false;
         'bg-zinc-100 dark:bg-zinc-900': addBgClasses,
       }"
     >
-      <h1 class="text-xl font-thin text-zinc-300 mb-4">Injective Peggy</h1>
+      <InjectiveLogo class="h-10 mx-auto" />
+      <p class="text-center font-light text-sm -mt-2 text-gray-400 mb-6">
+        Peggy Bridge
+      </p>
 
       <div
-        class="grid grid-cols-2 gap-2 p-1 border dark:border-zinc-700 rounded-lg"
+        class="grid grid-cols-2 p-1 border border-zinc-300 dark:border-zinc-700 rounded-lg"
       >
         <button
           v-for="value in ['deposit', 'withdraw']"
           class="px-2 py-2 text-sm font-semibold w-full rounded capitalize"
           @click="selectedView = value"
           :class="{
-            'bg-blue-400 text-black': selectedView === value,
+            'bg-blue-200 dark:bg-blue-400 text-black': selectedView === value,
           }"
         >
           {{ value }}
